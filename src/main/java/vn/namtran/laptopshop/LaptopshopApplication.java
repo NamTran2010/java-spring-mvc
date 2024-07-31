5cb79e6b11cc6a5a0906e819555998e587144dab
NamTran2010 <trandinhnam20102001@gmail.com>
2024-07-31 18:11:02 +0700

    update file

--- src/main/java/vn/namtran/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/namtran/laptopshop/LaptopshopApplication.java
@@ -1,5 +1,6 @@
 package vn.namtran.laptopshop;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
 
+		// container
+		ApplicationContext namtran = SpringApplication.run(LaptopshopApplication.class, args);
+
+		for (String s : namtran.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
+	}
 }
---
+++ src/main/java/vn/namtran/laptopshop/services/MyService.java
@@ -0,0 +1,8 @@
+package vn.namtran.laptopshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+}
